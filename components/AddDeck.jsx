d3fb73960131498141eb26db25a9ac40af097534
Jeffrey Stewart <jstewartofficial20111@gmail.com>
2022-03-02 17:54:44 -0600

    Add deck modal and attach click functionality

--- components/AddDeck.jsx
+++ components/AddDeck.jsx
@@ -1,13 +1,22 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
-import React from 'react';
+import { React, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import DeckModal from './DeckModal';
 
 const AddDeck = () => {
+  const [open, setOpen] = useState(false);
+  const handleModalOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-      <div className="viewport" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+      <DeckModal open={open} setOpen={setOpen} setClose={handleClose} />
+      <div onClick={handleModalOpen} className="viewport" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
         <FontAwesomeIcon icon={faPlus} />
         <p>Add a Deck</p>
       </div>
--- components/DeckModal.jsx
+++ components/DeckModal.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/function-component-definition */
+import { React, useState } from 'react';
+import { Dialog, DialogTitle } from '@mui/material';
 
-const DeckModal = () => {
+const DeckModal = ({ open, setOpen, setClose }) => {
   return (
-    <div style={{ maxHeigth: '100%' }}>
-      <div className="viewport">
-        <img src={deck.image.src} alt="Deck of many Things" style={{ maxHeight: '100%', maxWidth: '100%' }} />
-        <h4>{deck.name}</h4>
-        <p>{deck.description}</p>
-      </div>
+    <div>
+      <Dialog open={open} onClose={setClose}>
+        <DialogTitle>Haiya</DialogTitle>
+        <div>Hello</div>
+      </Dialog>
     </div>
   );
 };
