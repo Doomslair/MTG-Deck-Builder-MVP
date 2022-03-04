d856d06707d67c2994fe4be07be9be97cb73a4bf
Jeffrey Stewart <jstewartofficial20111@gmail.com>
2022-03-03 19:28:56 -0600

    Add DeckModal

--- components/DeckModal.jsx
+++ components/DeckModal.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
 import { React, useState } from 'react';
-import { Dialog, DialogTitle } from '@mui/material';
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, TextField } from '@mui/material';
 
 const DeckModal = ({ open, setOpen, setClose }) => {
+  const handleAdd = () => {};
   return (
     <div>
       <Dialog open={open} onClose={setClose}>
-        <DialogTitle>Haiya</DialogTitle>
-        <div>Hello</div>
+        <DialogTitle>Add New Deck</DialogTitle>
+        <DialogContent>
+          <Button onClick={handleAdd}>Add</Button>
+        </DialogContent>
       </Dialog>
     </div>
   );
